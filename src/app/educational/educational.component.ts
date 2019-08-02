9f265149a00c849911bf26f22003a34cb5e69538
maxim <loginov@dev.picom.ru>
2019-08-02 13:55:43 +0400

    доработки формы

--- src/app/educational/educational.component.ts
+++ src/app/educational/educational.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {QuestionJson} from '../educationServices';
-import { NgForm} from '@angular/forms';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-educational',
@@ -12,16 +12,38 @@ export class EducationalComponent implements OnInit {
   const;
   questionJson: any;
   inputJson: any = [];
+  match: any;
+  question: number = 0;
+  arrLink: any;
+  name: string = 'question';
 
   constructor(private eqJson: QuestionJson) {
   }
 
+
   submit(form: NgForm) {
     console.log(form.value);
   }
-  sort(arr){
-    for(let i = 0; i < arr.length; i++){
-      if(arr[i].active == true){
+
+  sort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].active == true) {
+        if (arr[i].type !== 'checkbox') {
+          this.arrLink = arr[i]
+        } else {
+          this.arrLink = arr[i].questionList
+        }
+        if (this.arrLink.name == undefined || this.arrLink.name.length == 0) {
+          if (this.arrLink.length > 1) {
+            for (let item = 0; item < 2; item++) {
+              this.question++;
+              this.arrLink[item].name = name+ this.question;
+            }
+          } else {
+            this.question++;
+            this.arrLink.name = name+ this.question;
+          }
+        }
         this.inputJson.push(arr[i]);
       }
     }
