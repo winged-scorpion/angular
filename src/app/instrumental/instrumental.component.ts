1f05cca13e5492345be4d7ea078302ab49b5f1e6
maxim <loginov@dev.picom.ru>
2019-09-03 17:55:58 +0400

    фнп калькулятор

--- src/app/instrumental/instrumental.component.ts
+++ src/app/instrumental/instrumental.component.ts
@@ -14,12 +14,52 @@ declare let require: any;
 })
 
 export class InstrumentalComponent implements OnInit {
-
+  numOut: string;
+  numIn: string;
+  plus: number;
+  el: any;
 
 
   ngOnInit() {
+    this.el = document.getElementsByClassName('js-im');
+
   }
 
+  number(item) {
+    if (typeof item === 'number') {
+      if (this.el[0].dataset.num1.indexOf('.') === -1) {
+        this.numOut = this.el[0].dataset.num1 + item;
+      } else {
+        this.numOut = (this.el[0].dataset.num1 + item) * 1;
+      }
+    } else {
+      if (this.el[0].dataset.num1.indexOf('.') === -1) {
+        this.numOut = this.el[0].dataset.num1 + item;
+      }
+    }
+    this.num();
+  }
 
+  numberDelete() {
+    this.numOut = '';
+    this.num();
+  }
+
+  numberPlus = () => {
+    this.numOut = this.numOut + 8890;
+    this.num();
+
+  };
+  numberMinus = () => {
+    this.numOut = this.numOut - 8890;
+    this.num();
+  };
+  numberEnter = () => {
+
+  };
+
+  num() {
+    this.el[0].dataset.num1 = this.numOut;
+  }
 }
 
